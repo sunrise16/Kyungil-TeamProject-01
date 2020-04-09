b52e41204294596703660764e787cbdb1245078b
sunrise16 <sunrise16@naver.com>
2020-04-09 09:29:57 +0900

    코드 추가 (적 이동)

---
+++ 홍준원/홍준원_코드1/inGameScene.cpp
@@ -0,0 +1,32 @@
+#include "inGameScene.h"
+
+HRESULT inGameScene::init()
+{
+	//문어 초기화
+	_octopus = new octopus;
+	_octopus->init();
+
+	return S_OK;
+}
+
+void inGameScene::release()
+{
+
+	_octopus->release();
+	//SAFE_DELETE(_octopus);
+	//문어 소멸
+}
+
+void inGameScene::update()
+{
+
+	//문어 업데이트
+	_octopus->update();
+}
+
+void inGameScene::render()
+{
+
+	//문어 렌더
+	_octopus->render();
+}
